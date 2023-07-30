7473676880cf35eeda8d9c5d9bb5a7aed1981e3b
Sanart99 <Sanart99@protonmail.com>
2023-07-30 02:00:26 +0200

    set_notification_to_read: +error case

--- public_html/lib/records.php
+++ public_html/lib/records.php
@@ -20,6 +20,8 @@ function set_notification_to_read(LDPDO $conn, int $userId, string $recordId) {
     $stmt = $conn->prepare('SELECT notified_ids,readnotifs_ids FROM records WHERE id=?');
     $stmt->execute([$recordId]);
     $recordRow = $stmt->fetch(\PDO::FETCH_ASSOC);
+    if ($recordRow == false) { release_lock($conn,$lockName); return ErrorType::NOTFOUND; }
+
     $notifiedIds = new Set(json_decode($recordRow['notified_ids'],null,512,JSON_OBJECT_AS_ARRAY));
     $readNotifsIds = new Set(json_decode($recordRow['readnotifs_ids'],null,512,JSON_OBJECT_AS_ARRAY));
     if (!$notifiedIds->contains($userId)) return ErrorType::USER_INVALID;
