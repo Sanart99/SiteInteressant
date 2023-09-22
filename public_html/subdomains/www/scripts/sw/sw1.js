edacbc233f84fb4fa6f0ad13877fef31dd9dfb8b
Sanart99 <Sanart99@protonmail.com>
2023-09-22 22:45:59 +0200

    +service worker

---
+++ public_html/subdomains/www/scripts/sw/sw1.js
@@ -0,0 +1,67 @@
+<?php
+header('Content-Type: text/javascript');
+
+$urlRegex1 = '/^\/(?:(?:index|forum|home|versionhistory)(?:\.php)?|style\.css|styleReset\.css)$/';
+$urlRegex2 = '/^\/scripts\/(?:init|load|quick|router|storage)\.js$/';
+$urlRegex3 = '/^\/scripts\/gen\/(?:main|popup)\.js$/';
+$urlRegex4 = '/^\/pages\/(?:forum|home|versionhistory)(?:\.php)?$/';
+$trimRegex = '/^(.*)\.php$/';
+
+echo <<<JAVASCRIPT
+const swName = 'sw1_v1.0';
+const __feat_cacheStorage = 'caches' in self;
+
+self.addEventListener('install', (event) => {
+    if (!__feat_cacheStorage) return;
+    event.waitUntil(caches.open(swName).then(c => c.addAll([
+        '/pages/forum',
+        '/pages/home',
+        '/pages/versionhistory',
+        '/scripts/gen/main.js',
+        '/scripts/gen/popup.js',
+        '/scripts/init.js',
+        '/scripts/load.js',
+        '/scripts/quick.js',
+        '/scripts/router.js',
+        '/scripts/storage.js',
+        '/index',
+        '/style.css',
+        '/styleReset.css',
+        '/forum',
+        '/home',
+        '/versionhistory'
+    ])));
+});
+
+self.addEventListener('activate', (event) => {
+    if (!__feat_cacheStorage) return;
+
+    const keep = [swName];
+    event.waitUntil((async () => {
+        const keys = await caches.keys();
+        const promises = [];
+        for (const k of keys) for (const s of keep) if (k != s) promises.push(caches.delete(k));
+        await Promise.all(promises);
+    })());    
+});
+
+self.addEventListener('fetch', (event) => {
+    if (!__feat_cacheStorage) return;
+
+    const url = new URL(event.request.url);
+    if (
+        $urlRegex1.exec(url.pathname) != null || $urlRegex2.exec(url.pathname) != null ||
+        $urlRegex3.exec(url.pathname) != null || $urlRegex4.exec(url.pathname) != null
+    ) {
+        event.respondWith((async () => {
+            const c = await caches.open(swName);
+            const vTrim = $trimRegex.exec(url.pathname);
+            let res = null;
+            if (vTrim != null) res = await c.match(vTrim[1]);
+            else res = await c.match(url.pathname);
+            return res == null ? fetch(url.href) : res;
+        })());
+    }
+});
+JAVASCRIPT;
+?>
