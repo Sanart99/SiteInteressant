738ae71cf1b48c89370cb251faad0e4276400dfe
Sanart99 <Sanart99@protonmail.com>
2023-10-28 05:50:30 +0200

    quick.js: +getDateAsString2

--- public_html/subdomains/www/scripts/quick.js
+++ public_html/subdomains/www/scripts/quick.js
@@ -58,6 +58,28 @@ function getDateAsString(date) {
     return a2;
 }
 
+function getDateAsString2(date) {
+    let s = '';
+    const milliDiff = Date.now()-date.getTime();
+    const secondsDiff = milliDiff / 1000;
+    if (secondsDiff < 60) return "Il y a moins d'une minute";
+    const minutesDiff = secondsDiff / 60;
+    if (minutesDiff < 60) return `Il y a \${Math.floor(minutesDiff)} minute\${minutesDiff < 2 ? '' : 's'}`;
+    const hoursDiff = minutesDiff / 60;
+    if (hoursDiff < 24) {
+        s = `Il y a \${Math.floor(hoursDiff)} heure\${hoursDiff < 2 ? '' : 's'}`;
+        let rest = Math.floor(minutesDiff%60);
+        if (rest != 0) s += ` et \${rest} minute\${rest < 2 ? '' : 's'}`
+        return s;
+    }
+
+    const nowDate = new Date(Date.now());
+    const daysDiff = hoursDiff / 24;
+    const sDate = getDateAsString(date);
+    if (nowDate.getDate() == date.getDate() + 1) return `Hier à \${sDate[4].substr(0,5)}`;
+    return new Intl.DateTimeFormat('fr-FR', { dateStyle: 'medium', timeStyle:'medium'}).format(date);
+}
+
 async function isServerInTestMode() {
     return await sendQuery(`query { testMode }`).then((json) => json?.data?.testMode == true);
 }
